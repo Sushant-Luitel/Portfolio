ece6822d0bbbf020b52ed8278c66aee47c7ff151
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-09 22:17:38 +0545

    created sidebar with animation

--- src/Components/Sidebar/Links.jsx
+++ src/Components/Sidebar/Links.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 const Links = () => {
   const sidebarItems = ["Home", "Services", "Contact", "Portfolio", "About"];
-  return <div>{sidebarItems.map((item, index) => {})}</div>;
+  return (
+    <div className="Links">
+      {sidebarItems.map((item, index) => {
+        return (
+          <a key={item} href="#">
+            {item}
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Links;
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.scss";
+import Links from "./Links";
+import { motion } from "framer-motion";
+import close from "../../assets/close.png";
+import hamburger from "../../assets/hamburger.png";
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  const [open, setOpen] = useState(false);
+
+  const variants = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      trasition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+      <button
+        className="sidebar-button"
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        {open ? (
+          <img src={close} width={20} alt="logo" />
+        ) : (
+          <img src={hamburger} width={20} alt="logo" />
+        )}
+      </button>
+    </motion.div>
+  );
 };
 export default Sidebar;
 Sidebar;
