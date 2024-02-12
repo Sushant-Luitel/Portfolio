db098a7097509c886f1d3226ccdbd255276bf348
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-12 15:54:43 +0545

    animated Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,24 +2,57 @@ import React from "react";
 import profile from "../../assets/profilep.png";
 import scroll from "../../assets/scroll.png";
 import "./Hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 const Hero = () => {
   return (
     <div className="Hero">
       <div className="container row">
-        <div className="hero-text">
-          <h2>Sushant Luitel</h2>
+        <motion.div
+          className="hero-text"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Sushant Luitel</motion.h2>
 
-          <h1>
+          <motion.h1 variants={textVariants}>
             Frontend Web <br />
             Developer(ReactJS)
-          </h1>
+          </motion.h1>
 
-          <div className="buttons">
-            <button className="btn-navy">See the Latest Works</button>
-            <button className="btn-white">Contact me</button>
-          </div>
-          <img className="scroll-image" src={scroll} alt="scroll" width={30} />
-        </div>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button className="btn-navy" variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button className="btn-whitevariants={textVariants}  ">
+              Contact me
+            </motion.button>
+          </motion.div>
+          <motion.img
+            className="scroll-image"
+            variants={textVariants}
+            src={scroll}
+            alt="scroll"
+            width={30}
+          />
+        </motion.div>
+        <div className="sliding-text-container">Developer Content Creator</div>
         <div className="image-container">
           <img src={profile} alt="profile" />
         </div>
